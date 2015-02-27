b1a5ecf066013643f0d63ea74aff2d133cd6951d
bayer106 <bayer106@morris.umn.edu>
2015-02-27 15:47:53 -0600

    added data to seed.js file for student list

--- server/api/student/student.model.js
+++ server/api/student/student.model.js
@@ -6,23 +6,7 @@ var mongoose = require('mongoose'),
 var StudentSchema = new Schema({
   "firstName": String,
   "lastName": String,
-  "dateOfBirth": String,
-  "gender": String,
-  "email": String,
-  "phone": String,
-  "address": String,
-  "courses": [
-    {
-      "course": {
-        "name": String,
-        "subject": String,
-        "courseNumber": Number,
-        "credits": Number
-      },
-      "grade": String
-    }],
-  "major1":String,
-  "major2": String
+  "gender": String
 });
 
 module.exports = mongoose.model('Student', StudentSchema);
