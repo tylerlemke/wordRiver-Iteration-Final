6937f22bc57f7e52cb9292cf2e35db09b4466948
BDenzer <denz0045@morris.umn.edu>
2015-03-03 13:59:20 -0500

    Context view can now display context packs with their words, it is only a local database with strings though.

--- client/app/contextview/contextview.controller.js
+++ client/app/contextview/contextview.controller.js
@@ -3,7 +3,22 @@
 angular.module('ummWordRiverTeam1Iteration1App')
   .controller('ContextviewCtrl', function ($scope) {
     $scope.packs = [
-      {name:"Pack 1"},
-      {name:"Pack 2"}
+      {name:"Greetings Pack",
+      words: ["Hi", "Hello"]
+      },
+      {name:"Animal",
+      words: ["Cat", "Dog"]}
     ];
+
+    $scope.showdetails = function(pack){
+      document.getElementById("coursesInfo").innerHTML = "";
+      //document.getElementById("studentInfo").innerHTML = "";
+      var words = "";
+      for(var i = 0; i < pack.words.length; i++) {
+          words = words + pack.words[i] + "<br>";
+
+      }
+      document.getElementById("coursesInfo").innerHTML = "<u>" + pack.name  + "</u><br/>"
+                                                        + words;
+    };
   });
