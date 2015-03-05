f7161468c68869c3a6bab3d348cc2f0a7e9d3fb5
tylerlemke <lemke164@umn.edu>
2015-03-05 12:56:27 -0600

    Fixed error that occurred when navigating to the Home page by removing all unused code in main.controller and main.controller.spec.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,31 +2,5 @@
 
 angular.module('wordRiverSpaceshipParrotIteration1App')
   .controller('MainCtrl', function ($scope, $http, socket) {
-    $scope.awesomeThings = [];
-
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('thing', $scope.awesomeThings);
-    });
-
-    $http.get('/api/students').success(function(awesomeStudents) {
-      $scope.awesomeThings = awesomeStudents;
-      socket.syncUpdates('student', $scope.studentList);
-    });
-
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
-
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
-
-    $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('thing');
-    });
-  });
+        // Put stuff here if you want
+});
