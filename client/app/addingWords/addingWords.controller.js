8d145b643717d0438f3ae3beda9d83b40095125d
xiaox232 <xiaox232@morris.umn.edu>
2015-03-05 14:58:03 -0600

    We added students to our add words page with check marks so we can later pick and choose students to give words to. The table lets you sort by groups (grades). when the grades are clicked, the students in that group show up.

--- client/app/addingWords/addingWords.controller.js
+++ client/app/addingWords/addingWords.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('wordRiverTeamFtlApp')
-  .controller('AddingWordsCtrl', function ($scope, $http, socket) {
+  .controller('AddingWordsCtrl', function ($rootScope, $scope, $http, socket) {
     $scope.currentWords = [];
     $scope.allWords = [];
 
@@ -28,7 +28,6 @@ angular.module('wordRiverTeamFtlApp')
           $http.post('/api/AddingWordsDatabases', {words: $scope.wordField}).success(function () {
             $scope.getWords();
             $scope.allWords.push({words: $scope.wordField});
-            console.log($scope.allWords.length);
           });
           $scope.wordField = "";
       }
@@ -54,5 +53,74 @@ angular.module('wordRiverTeamFtlApp')
         $scope.getWords();
       });
     };
+//=========================================================================
+    $scope.students = [];
+    $scope.classList = [];
+    $scope.studentList = [];
+    $scope.studentSortArray = [];
+    $scope.filterText = null;
+    $rootScope.currentStudent = null;
 
-  });
+
+    $http.get('/api/things').success(function(students) {
+      $scope.students = students;
+      socket.syncUpdates('thing', $scope.students);
+      $scope.totalClasses();
+      $scope.populateStudentArray();
+    });
+
+    $scope.addThing = function() {
+      if($scope.newThing === '') {
+        return;
+      }
+      $http.post('/api/things', { name: $scope.newThing });
+      $scope.newThing = '';
+    };
+
+    $scope.deleteThing = function(thing) {
+      $http.delete('/api/things/' + thing._id);
+    };
+
+    $scope.$on('$destroy', function () {
+      socket.unsyncUpdates('thing');
+    });
+
+    //creates a list of all classes that exist
+    $scope.totalClasses = function(){
+      for(var i=0; i<$scope.students.length;i++){
+        var found = false;
+        for(var j=0; j<=$scope.classList.length; j++){
+          if($scope.classList[j]==$scope.students[i].class){
+            found = true;
+          }
+        }
+        if (!found){
+          $scope.classList.push($scope.students[i].class);
+        }
+      }
+    };
+
+    //creates a list of students
+    $scope.populateStudentArray = function(){
+      for(var i=0; i<$scope.students.length; i++){
+        for(var j=0; j<$scope.classList.length; j++){
+          if($scope.students[i].class == $scope.classList[j]){
+            var name = $scope.students[i].firstName + " " + $scope.students[i].lastName;
+            $scope.studentList.push({student: name, course: $scope.classList[j]});
+          }
+        }
+      }
+    };
+
+    //changes the class view
+    $scope.changeFilter = function(str){
+      $scope.filterText = str;
+    };
+
+    $scope.makeCurrentStudent = function(student){
+      $rootScope.currentStudent = student;
+    };
+
+  //});
+
+});
