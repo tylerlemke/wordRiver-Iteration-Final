bb30a2878754ebfd3bd263c250e0b6ed3afcd366
hauge550 <hauge550@morris.umn.edu>
2015-03-06 16:31:33 -0600

    values are stored with checked boxes. and the system knows which check boxes are checked

--- client/app/addingWords/addingWords.controller.js
+++ client/app/addingWords/addingWords.controller.js
@@ -5,6 +5,9 @@ angular.module('wordRiverTeamFtlApp')
     $scope.currentWords = [];
     $scope.allWords = [];
 
+    $scope.checkedWords=[];
+    $scope.checkedStudents=[];
+
     $scope.wordField = "";
 
     //beforeEach(module('wordRiverTeamFtlApp'));
@@ -19,11 +22,8 @@ angular.module('wordRiverTeamFtlApp')
 
     $scope.getWords();
 
-  //When going to the page for the first time, you have to submit something before all the previously added words will show up
     $scope.addWords = function(){
-      //console.log("this is before the if statement. checking the hasDuplicateValues function " + $scope.hasDuplicateValues($scope.wordField, $scope.allWords));
       if($scope.wordField.length >= 1) {
-        //console.log("this is after the if statement. checking the hasDuplicateValues function " + $scope.hasDuplicateValues($scope.wordField, $scope.allWords));
           <!--these words will be going into the individuals page, possibly the class words, and added to her program (words they can use) -->
           $http.post('/api/AddingWordsDatabases', {words: $scope.wordField}).success(function () {
             $scope.getWords();
@@ -52,6 +52,45 @@ angular.module('wordRiverTeamFtlApp')
       $http.delete('/api/AddingWordsDatabases/' + $scope.allWords[index]._id).success(function(){
         $scope.getWords();
       });
+    };
+
+    $scope.checkAllCheckboxes = function(field){
+      for (var i = 0; i < field.length; i++){
+        field[i].checked = true;
+      }
+    };
+
+    $scope.unCheckAllCheckboxes = function(field){
+      for (var i = 0; i < field.length; i++){
+        field[i].checked = false;
+      }
+    };
+
+    $scope.isChecked = false;
+    $scope.studentChecked = false;
+
+    $scope.allCheckedWords = function(object){
+      object.value = !object.value;
+      if(object.value==!$scope.isChecked){
+        console.log('true');
+        $scope.checkedWords.push(object.words);
+        console.log(object.words);
+      }
+      else if(object.value == false){
+        console.log("false");
+      }
+    };
+
+    $scope.allCheckedStudents = function(object){
+      object.value = !object.value;
+      if(object.value == !$scope.studentChecked){
+        console.log('true');
+        $scope.checkedStudents.push(object.student);
+        console.log(object.student);
+      }
+        else if(object.value == false){
+           console.log("false");
+       }
     };
 //=========================================================================
     $scope.students = [];
