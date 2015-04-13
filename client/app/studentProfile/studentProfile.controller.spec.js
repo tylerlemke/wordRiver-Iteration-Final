9a6de403a2211fe47c36d922e8c7c899c574a4a4
xiaox232 <xiaox232@morris.umn.edu>
2015-04-13 15:25:08 -0500

    We added studentProfile route. (Molly and Yuting)

---
+++ client/app/studentProfile/studentProfile.controller.spec.js
@@ -0,0 +1,21 @@
+'use strict';
+
+describe('Controller: StudentProfileCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('wordRiverAppApp'));
+
+  var StudentProfileCtrl, scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    StudentProfileCtrl = $controller('StudentProfileCtrl', {
+      $scope: scope
+    });
+  }));
+
+  it('should ...', function () {
+    expect(1).toEqual(1);
+  });
+});
